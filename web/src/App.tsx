531a7258cdf71ca76157c0be447311bb4a051314
Alanio <alaniocarvalho.cs@gmail.com>
2020-06-04 23:53:42 -0300

    Frontend: React Concepts

---
+++ web/src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./App.css";
+
+import Header from "./Header";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+  return (
+    <div>
+      <Header title={`Contador: ${counter}`} />
+
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
+  );
+}
+
+export default App;
